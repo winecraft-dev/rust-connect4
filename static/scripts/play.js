9395a70c3d24b237308db796c38651d260abbfd2
Joseph Norman <jnormantransactions@gmail.com>
2026-03-03 13:15:54 -0500

    main gameplay from frontend

--- static/scripts/play.js
+++ static/scripts/play.js
@@ -3,11 +3,18 @@ window.onload = function (e) {
   let username_field = document.getElementById("username");
   let connection_status = document.getElementById("status");
 
+  let drop_buttons = document.querySelectorAll(".drop-button");
+  console.log(drop_buttons);
+
   let gameplay_text = document.getElementById("gameplay");
 
   let socket = null;
 
   function drop_chip(column) {
+    if (socket == null) {
+      console.error("cannot drop chip when socket is closed");
+      return;
+    }
     socket.send(
       JSON.stringify({
         type: "DropChip",
@@ -19,9 +26,16 @@ window.onload = function (e) {
   function handle_message(msg) {
     console.log(`Received message:`);
     console.log(msg);
-    if (typeof msg.board !== undefined) {
+    if (msg.board != null) {
       gameplay_text.value = msg.board;
     }
+  }
+
+  function buttons_connect(connected) {
+    connect_button.disabled = connected;
+    drop_buttons.forEach(function (drop_button) {
+      drop_button.disabled = !connected;
+    });
   }
 
   function connect(username) {
@@ -30,6 +44,7 @@ window.onload = function (e) {
 
     socket.onopen = function (e) {
       connection_status.classList.add("status-online");
+      buttons_connect(true);
       console.log("Connected!");
     };
 
@@ -41,6 +56,7 @@ window.onload = function (e) {
     socket.onclose = function (e) {
       connection_status.classList.remove("status-online");
       console.log("Disconnected");
+      buttons_connect(false);
     };
 
     socket.onerror = function (e) {
@@ -51,5 +67,11 @@ window.onload = function (e) {
   connect_button.addEventListener("click", function (e) {
     let username = username_field.value;
     connect(username);
+  });
+
+  drop_buttons.forEach(function (drop_button) {
+    drop_button.addEventListener("click", function (e) {
+      drop_chip(parseInt(e.target.getAttribute("column")));
+    });
   });
 };
