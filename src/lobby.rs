94f87a42626193c3a65ef6b7833a4070ada4efeb
Joseph Norman <jnormantransactions@gmail.com>
2026-03-07 14:15:25 -0500

    match is made, prepare to start game

--- src/lobby.rs
+++ src/lobby.rs
@@ -1,8 +1,12 @@
 use std::collections::HashMap;
 
+use rand::random_bool;
 use thiserror::Error;
 
-use crate::connection::{ConnRx, Connection, ConnectionUpdate};
+use crate::{
+    connection::{ConnRx, Connection, ConnectionUpdate},
+    game::Game,
+};
 
 #[derive(Debug, Error)]
 pub enum LobbyError {
@@ -14,6 +18,11 @@ pub enum LobbyError {
 pub struct Lobby {
     conn_rx: ConnRx,
     connecting: HashMap<String, Connection>,
+}
+
+struct Match {
+    red: Connection,
+    blue: Connection,
 }
 
 impl Lobby {
@@ -32,13 +41,18 @@ impl Lobby {
         match cu {
             ConnectionUpdate::Connected(conn) => {
                 let username = conn.username.clone();
-                println!("[Lobby-CU] Player connecting: ${}", &username);
+                println!("[Lobby] Player connecting: {}", &username);
                 self.connecting.insert(username, conn);
-                self.matchmake();
+                if let Some(mmatch) = self.matchmake() {
+                    println!(
+                        "[Lobby] Match made! Red: {}, Blue: {}",
+                        &mmatch.red.username, &mmatch.blue.username,
+                    );
+                }
             }
             ConnectionUpdate::Disconnected(username) => {
                 if let Some(_) = self.connecting.remove(&username) {
-                    println!("[Lobby-CU] Player disconnected: ${}", username);
+                    println!("[Lobby] Player disconnected: {}", username);
                 }
             }
         }
@@ -46,16 +60,26 @@ impl Lobby {
         Ok(())
     }
 
-    fn matchmake(&mut self) {
-        if self.connecting.len() < 2 {
-            return;
-        }
-        let players = Vec::<Connection>::new();
-        let mut player_iter = self.connecting.keys();
-        if let Some(username) = player_iter.next() {
-            self.connecting.remove_entry(username);
-        } else {
-        }
+    fn matchmake(&mut self) -> Option<Match> {
+        let mut usernames = self.connecting.keys().cloned();
+        let u1 = usernames.next();
+        let u2 = usernames.next();
+        let (Some(u1), Some(u2)) = (u1, u2) else {
+            return None;
+        };
+
+        let c1 = self.connecting.remove(&u1);
+        let c2 = self.connecting.remove(&u2);
+        let (Some(c1), Some(c2)) = (c1, c2) else {
+            unreachable!(); // we should panic cause this is impossible
+        };
+
+        let (red, blue) = match random_bool(1.0 / 2.0) {
+            true => (c1, c2),
+            false => (c2, c1),
+        };
+
+        return Some(Match { red, blue });
     }
 }
 
